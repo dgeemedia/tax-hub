9b6aa444829cb1b9f44b32516fd73ad9bd7b6a19
dgeemedia <lolumah@byupathway.edu>
2025-12-21 17:06:05 +0100

    ok

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -1,4 +1,3 @@
-// apps/web/src/lib/auth.ts
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@auth/prisma-adapter"
@@ -38,35 +37,48 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           return null
         }
 
+        // Return a minimal user object that matches NextAuth's User type
         return {
           id: user.id,
           email: user.email,
           name: user.firstName && user.lastName 
             ? `${user.firstName} ${user.lastName}` 
             : user.email,
-          userType: user.userType,
-          firstName: user.firstName,
-          lastName: user.lastName
+          // Add extra properties as needed - they'll be available in the token
         }
       }
     })
   ],
   callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id
-        token.userType = user.userType
-        token.firstName = user.firstName
-        token.lastName = user.lastName
+    async jwt({ token, user, account }) {
+      // Initial sign in
+      if (account && user) {
+        // Fetch the full user to get additional fields
+        const dbUser = await prisma.user.findUnique({
+          where: { id: user.id }
+        })
+        
+        if (dbUser) {
+          token.id = dbUser.id
+          token.email = dbUser.email
+          token.userType = dbUser.userType
+          token.firstName = dbUser.firstName
+          token.lastName = dbUser.lastName
+          token.name = dbUser.firstName && dbUser.lastName 
+            ? `${dbUser.firstName} ${dbUser.lastName}`
+            : dbUser.email
+        }
       }
       return token
     },
     async session({ session, token }) {
       if (session.user) {
         session.user.id = token.id as string
+        session.user.email = token.email as string
+        session.user.name = token.name as string
         session.user.userType = token.userType as string
-        session.user.firstName = token.firstName as string | undefined
-        session.user.lastName = token.lastName as string | undefined
+        session.user.firstName = token.firstName as string | null | undefined
+        session.user.lastName = token.lastName as string | null | undefined
       }
       return session
     }
